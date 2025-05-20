3c285562aee78a23bb5edcd2d2345cd7a1ca8308
christian <Christianmnaira@gmail.com>
2025-05-20 22:56:20 +0800

    feat&fix: added viewbinding

--- app/src/main/java/com/example/myminibrain/UserStartActivity.java
+++ app/src/main/java/com/example/myminibrain/UserStartActivity.java
@@ -1,6 +1,8 @@
 package com.example.myminibrain;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myminibrain.databinding.ActivityUserStartBinding;
+
 public class UserStartActivity extends AppCompatActivity {
+
+    ActivityUserStartBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityUserStartBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_start);
+        setContentView(binding.getRoot());
+
+        binding.sellBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(UserStartActivity.this, "sell", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.buyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(UserStartActivity.this, "buy", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
